5fe5c787a986278b84ecfd9790b8eb5cbc24787c
Olli Paloviita <olli.paloviita@gmail.com>
2021-11-18 15:50:23 +0200

    Ticket expiring logic implemented

--- app/src/main/java/com/ticketapp/auth/ticket/Ticket.java
+++ app/src/main/java/com/ticketapp/auth/ticket/Ticket.java
@@ -18,10 +18,15 @@ import java.util.Date;
  */
 public class Ticket {
     /** CUSTOM CONSTANTS */
-    private final int LEFT_PAGE_TITLE = 6;
-    private final int LEFT_PAGE_AMOUNT = 7;
-    private final int ISSUE_AMOUNT = 5;
+    private final int LEFT_TITLE_PAGE = 6;
+    private final int LEFT_AMOUNT_PAGE = 7;
+    private final int EXPIRE_TITLE_PAGE = 8;
+    private final int EXPIRE_DATE_PAGE = 9;
     private final String LEFT_TITLE = "left";
+    private final String EXPIRE_TITLE = "expr";
+    private final String EXPIRE_NOT_STARTED = "TBA-";
+    private final int EXPIRE_TIME_MIN = 1;
+    private final int ISSUE_AMOUNT = 5;
 
     /** Default keys are stored in res/values/secrets.xml **/
     private static final byte[] defaultAuthenticationKey = TicketActivity.outer.getString(R.string.default_auth_key).getBytes();
@@ -74,36 +79,59 @@ public class Ticket {
         Utilities.log("issue()", true);
 
         String currentFailMsg = "";
+        boolean wasExpired;
         try { // NOTE: every method starting with 'try' can raise Exception
             // Authenticate
             currentFailMsg = "Authentication failed";
             tryAuthenticate();
 
+            // Formatting:
             // Add "left" title if needed
             currentFailMsg = "Left title reading failed";
-            String leftTitle = tryReadPage(LEFT_PAGE_TITLE);
+            String leftTitle = tryReadPage(LEFT_TITLE_PAGE);
             if (!leftTitle.equals(LEFT_TITLE)) {
                 currentFailMsg = "Left title writing failed";
-                tryWritePage(LEFT_PAGE_TITLE, LEFT_TITLE);
+                tryWritePage(LEFT_TITLE_PAGE, LEFT_TITLE);
+            }
+            // Add "expr" title if needed
+            currentFailMsg = "Expire title reading failed";
+            String exprTitle = tryReadPage(EXPIRE_TITLE_PAGE);
+            if (!exprTitle.equals(EXPIRE_TITLE)) {
+                currentFailMsg = "Expire title writing failed";
+                tryWritePage(EXPIRE_TITLE_PAGE, EXPIRE_TITLE);
             }
 
             // Read how many uses left, initialize with 0 if not set
             currentFailMsg = "Reading left amount failed";
-            String leftAmountString = tryReadPage(LEFT_PAGE_AMOUNT);
+            String leftAmountString = tryReadPage(LEFT_AMOUNT_PAGE);
             int leftAmount;
             try {
                 currentFailMsg = "Converting left amount failed";
                 leftAmount = tryStringToInt(leftAmountString);
             } catch(java.lang.NumberFormatException e) {
                 currentFailMsg = "Initial left amount writing failed";
-                tryWritePage(LEFT_PAGE_AMOUNT, "0000");
+                tryWritePage(LEFT_AMOUNT_PAGE, "0000");
                 leftAmount = 0;
             }
 
+            // Calculate new amount based on if expired or not
+            currentFailMsg = "Expire read failed";
+            int newAmount;
+            if (hasExpired(tryReadBytes(EXPIRE_DATE_PAGE))) {
+                wasExpired = true;
+                newAmount = ISSUE_AMOUNT;
+            } else {
+                wasExpired = false;
+                newAmount = leftAmount + ISSUE_AMOUNT;
+            }
+
+            // Reset expire
+            currentFailMsg = "Expire date write failed";
+            tryWritePage(EXPIRE_DATE_PAGE, EXPIRE_NOT_STARTED);
+
             // Issue new
             currentFailMsg = "Left amount writing failed";
-            int newAmount = leftAmount + ISSUE_AMOUNT;
-            tryWritePage(LEFT_PAGE_AMOUNT, intToPageString(newAmount));
+            tryWritePage(LEFT_AMOUNT_PAGE, intToPageString(newAmount));
 
             // Update state
             remainingUses = newAmount;
@@ -113,7 +141,15 @@ public class Ticket {
             isValid = false;
             return false;
         }
-        logErrorAndInfo("Issue success! Uses left:" + remainingUses);
+
+        if (!wasExpired) {
+            logErrorAndInfo("Issue success! Uses left: " + remainingUses);
+        } else {
+            logErrorAndInfo(
+                    "Issue success! Uses left: " + remainingUses +
+                    " (old tickets had expired or did not exist)"
+            );
+        }
         return true;
     }
 
@@ -133,7 +169,7 @@ public class Ticket {
 
             // Read how many uses left
             currentFailMsg = "Reading left amount failed";
-            String leftAmountString = tryReadPage(LEFT_PAGE_AMOUNT);
+            String leftAmountString = tryReadPage(LEFT_AMOUNT_PAGE);
             int leftAmount;
             try {
                 currentFailMsg = "Converting left amount failed";
@@ -148,10 +184,28 @@ public class Ticket {
                 throw new Exception("not valid");
             }
 
+            // Check if expired
+            // Calculate new amount based on if expired or not
+            currentFailMsg = "Expire read failed";
+            byte[] exprBytes = tryReadBytes(EXPIRE_DATE_PAGE);
+            if (hasExpired(exprBytes)) {
+                currentFailMsg = "Tickets expired";
+                throw new Exception("expired");
+            }
+
+            // Start expire countdown if not started yet
+            if (!hasStarted(exprBytes)) {
+                currentFailMsg = "Expire write failed";
+                byte[] expireBytes = ByteBuffer.allocate(4).putInt(
+                        currentDateMinInt() + EXPIRE_TIME_MIN
+                ).array();
+                tryWriteBytes(EXPIRE_DATE_PAGE, expireBytes);
+            }
+
             // Use
             currentFailMsg = "Left amount writing failed";
             int newAmount = leftAmount - 1;
-            tryWritePage(LEFT_PAGE_AMOUNT, intToPageString(newAmount));
+            tryWritePage(LEFT_AMOUNT_PAGE, intToPageString(newAmount));
 
             // Update state
             remainingUses = newAmount;
@@ -175,19 +229,27 @@ public class Ticket {
         }
     }
 
-    private String tryReadPage(int page) throws Exception {
-        byte[] pageBytes = new byte[4];
-        if(!utils.readPages(page, 1, pageBytes, 0)) {
-            throw new Exception("Page read failed");
+    private byte[] tryReadBytes(int page) throws Exception {
+        byte[] bytes = new byte[4];
+        if(!utils.readPages(page, 1, bytes, 0)) {
+            throw new Exception("Bytes read failed");
         }
-        return new String(pageBytes);
+        return bytes;
+    }
+
+    private void tryWriteBytes(int page, byte[] messageBytes) throws Exception {
+        if(!writePage(page, messageBytes)) {
+            throw new Exception("Page write failed");
+        }
+    }
+
+    private String tryReadPage(int page) throws Exception {
+        return new String(tryReadBytes(page));
     }
 
     private void tryWritePage(int page, String message) throws Exception {
         assert message.length() == 4: "Page length must be 4";
-        if(!writePage(page, message.getBytes())) {
-            throw new Exception("Page write failed");
-        }
+        tryWriteBytes(page, message.getBytes());
     }
 
     private void logErrorAndInfo(String message) {
@@ -195,13 +257,28 @@ public class Ticket {
         Utilities.log(message, true);
     }
 
-
     private String intToPageString(int Int) {
         return String.format("%04d", Int);
     }
 
     private int tryStringToInt(String page) {
         return Integer.parseInt(page);
+    }
+
+    private boolean hasStarted(byte[] dateBytes) {
+        return !(new String(dateBytes).equals(EXPIRE_NOT_STARTED));
+    }
+
+    private int currentDateMinInt() {
+        return (int) ((new Date()).getTime() / 1000 / 60);
+    }
+
+    private boolean hasExpired(byte[] dateBytes) {
+        if (!hasStarted(dateBytes)) {
+            return false;
+        }
+        int dateMinInt = new BigInteger(dateBytes).intValue();
+        return dateMinInt < currentDateMinInt();
     }
 
     /**
